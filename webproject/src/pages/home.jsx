db298973082dc0a6b5ea9d66d94cf768570e3cd8
홍승환 <lap2006@chungbuk.ac.kr>
2026-05-03 04:10:21 +0900

    hero 레이아웃 작업, gitignore추가 했습니다여

--- webproject/src/pages/home.jsx
+++ webproject/src/pages/home.jsx
@@ -1,5 +1,21 @@
+import {NavLink} from 'react-router-dom';
+import './home.css';
+
+import homeFrameImg from '../assets/homeFrame.svg';
+
 function Home() {
-    return <div></div>
+    return (
+
+        <section className="home-hero">
+            <div className="home-hero-content">
+                <h1 className="home-hero-title">재난 상황에서 침착하게</h1>
+                <p className="home-hero-text">응급처치 · 대피소 찾기 · 재난 매뉴얼 · 의약품 정보 — 오프라인에서도 작동합니다</p>
+            </div>
+            <div className="home-hero-img">
+                <img src={homeFrameImg} alt="homeFrame"/>
+            </div>
+        </section>
+    );
 }
 
 export default Home;
