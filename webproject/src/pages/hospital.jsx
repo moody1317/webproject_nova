11d105b7f4664a7d1a6fb8281aca32f5229f718a
Shim soen <soen13@chungbuk.ac.kr>
2026-05-11 14:49:47 +0900

    틀 완성.

--- webproject/src/pages/hospital.jsx
+++ webproject/src/pages/hospital.jsx
@@ -1,19 +1,62 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import HospitalCard from '../components/hospitalCard';
+import 'leaflet/dist/leaflet.css';
 import './hospital.css';
 
 function Hospital() {
+    const [sortType, setSortType] = useState('distance');
+
     return (
         <>
-        <section className='hero'>
+        <section className='hospital-hero'>
             <div>
                 <h1>인근 병원·약국 찾기</h1>
                 <p>현재 위치 기준 가까운 병원, 응급실, 약국 정보를 실시간으로 제공합니다.</p>
             </div>
-            <div className='search'>
+            <div className='hospital-search'>
                 <i className='bi bi-search'></i>
                 <input type='text' id='txt-search' placeholder='진료 과목으로 검색...'></input>
                 <input type='button' id='btn-search' value='검색'></input>
             </div>
         </section>
+        <div className='hospital-content'>
+            <section className='hospital-map'>
+                <MapContainer center={[37.5665, 126.9780]} zoom={13} scrollWheelZoom={false} style={{height: '100%', width: '100%'}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; CARTO'
+                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    />
+                    <Marker position={[37.5665, 126.9780]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                    </MapContainer>
+            </section>
+            <section className='hospital-panel'>
+                <div className='hospital-panel-top'>
+                    <div className='hospital-panel-right'>
+                        <h1>전체</h1>
+                        <h3>00곳</h3>
+                    </div>
+                    <div className='hospital-panel-left'>
+                        <button className={sortType === 'distance' ? 'hospital-sort-btn active' : 'hospital-sort-btn'} onClick={() => setSortType('distance')}>거리순</button>
+                        <button className={sortType === 'treatment' ? 'hospital-sort-btn active' : 'hospital-sort-btn'} onClick={() => setSortType('treatment')}>진료중</button>
+                    </div>
+                </div>
+                <div  className='hospital-panel-list'>
+                    <HospitalCard
+                            placeType="hospital"
+                            name="서원의원"
+                            tagname="내과"
+                            distance="0.7km"
+                            time="09~18시"
+                            isOpen="Open"
+                        />
+                </div>
+            </section>
+        </div>
         </>
     )
 }
