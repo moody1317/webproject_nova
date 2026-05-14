17660b24cf2871759360f6c95a6a00221fa14112
Shim soen <soen13@chungbuk.ac.kr>
2026-05-14 15:01:52 +0900

    Update firstaidContent.jsx

--- webproject/src/components/firstaidContent.jsx
+++ webproject/src/components/firstaidContent.jsx
@@ -1,12 +1,37 @@
-import firstaid from '../data/firstaidData';
+import FirstaidCard from './firstaidCard';
 import './firstaidContent.css';
 
-function FirstaidContent({ selectedIndex }) {
+function FirstaidContent({ selectedIndex, selectedData, isOpen }) {
+    const stepsList = selectedData?.steps?.map((item, index) =>
+        <FirstaidCard
+            num = { item.number }
+            title = { item.title }
+            stepIcon = { item.stepIcon }
+            description = { item.content }
+            isOpen = { isOpen }
+            key = { index }
+        />
+    );
     
     return (
         <>
             <section className='firstaid-content'>
-                <h1>{ firstaid[selectedIndex].name }</h1>
+                <h1>{ selectedData?.name }</h1>
+                <p className='firstaid-explan'>설명 글</p>
+                <hr/>
+                <div className='firstaid-grid'>
+                    { stepsList }
+                </div>
+                <div className={isOpen ? 'warn-box closed' :'warn-box'}>
+                    <h3 className='warn-h3'><i className='bi bi-exclamation-triangle-fill'></i> 주의사항</h3>
+                    <p className='box-p'>{ selectedData?.warning }</p>
+                </div>
+                <div className={isOpen ? 'link-box closed' :'link-box'}>
+                    <div className='link-icon'>
+                        <i className='bi bi-play-fill'></i>
+                    </div>
+                    <a href={ selectedData?.videoUrl } target='_blank' className='box-p'>관련 영상 보기(링크)</a>
+                </div>
             </section>
         </>
     )
