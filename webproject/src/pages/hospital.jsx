4ecca6ebd85791c33088e5bb26b3dd4243c6e616
Shim soen <soen13@chungbuk.ac.kr>
2026-05-19 14:31:00 +0900

    의원 색 통일

--- webproject/src/pages/hospital.jsx
+++ webproject/src/pages/hospital.jsx
@@ -1,211 +1,111 @@
-.hospital-hero {
-    width: auto;
-    height: 116px;
-    background-color: var(--color-background);
-    border-left: 4px solid var(--color-line-primary);
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { useState, useEffect } from 'react';
+import L from 'leaflet';
+import HospitalCard from '../components/hospitalCard';
+import { currentMarker, hospitalMarker, pharmacyMarker, emergencyMarker, clinicMarker } from '../components/mapMarker';
+import 'leaflet/dist/leaflet.css';
+import './hospital.css';
+
+function MoveMap({ position }) {
+    useMap().setView(position, 20);
 }
 
-.hospital-hero h1 {
-    color: var(--color-text-primary);
-    margin-left: var(--spacing-xl);
+function Hospital() {
+    const [sortType, setSortType] = useState('distance');
+    const [curPosition, setCurPosition] = useState([37.5665, 126.9780]);
+    const [hospitals, setHospitals] = useState([]);
+    const [searchKeyword, setSearchKeyword] = useState('');
+
+    useEffect(() => { 
+        navigator.geolocation.getCurrentPosition( position => {
+            setCurPosition([position.coords.latitude, position.coords.longitude])
+        }, null, { enableHighAccuracy: true}) 
+    }, []);
+
+    useEffect(() => {
+        fetch(`/api/hospital?lat=${curPosition[0]}&lng=${curPosition[1]}`, {headers: {'ngrok-skip-browser-warning': 'true'}}).then(response => response.json()).then(data => setHospitals(data))
+        .catch(error => console.log(error));
+    }, [curPosition]);
+
+    const filteredHospital = hospitals.filter(item =>
+        item.tagname.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
+
+    const handleSearch = (event) => {
+        setSearchKeyword(event.target.value);
+    };
+
+    return (
+        <>
+        <section className='hospital-hero'>
+            <div>
+                <h1>인근 병원·약국 찾기</h1>
+                <p>현재 위치 기준 가까운 병원, 응급실, 약국 정보를 실시간으로 제공합니다.</p>
+            </div>
+            <div className='hospital-search'>
+                <i className='bi bi-search'></i>
+                <input type='text' id='txt-search' placeholder='진료 과목으로 검색...' onChange={ handleSearch }></input>
+                <input type='button' id='btn-search' value='검색'></input>
+            </div>
+        </section>
+        <div className='hospital-content'>
+            <section className='hospital-map'>
+                <div className='hospital-legend'>
+                    <ul className='hospital-marker-list'>
+                        <li>병원, 보건소, 종합병원</li>
+                        <li>약국</li>
+                        <li>응급실</li>
+                        <li>의원</li>
+                    </ul>
+                </div>
+                <MapContainer center={curPosition } zoom={20} scrollWheelZoom={false} style={{height: '100%', width: '100%'}} >
+                    <MoveMap position={ curPosition } />
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; CARTO'
+                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    />
+                    <Marker position={ curPosition } icon={ currentMarker }>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </section>
+            <section className='hospital-panel'>
+                <div className='hospital-panel-top'>
+                    <div className='hospital-panel-right'>
+                        <h1>전체</h1>
+                        <h3>00곳</h3>
+                    </div>
+                    <div className='hospital-panel-left'>
+                        <button className={sortType === 'distance' ? 'hospital-sort-btn active' : 'hospital-sort-btn'} onClick={() => setSortType('distance')}>거리순</button>
+                        <button className={sortType === 'treatment' ? 'hospital-sort-btn active' : 'hospital-sort-btn'} onClick={() => setSortType('treatment')}>진료중</button>
+                    </div>
+                </div>
+                <div  className='hospital-panel-list'>
+                    {filteredHospital.map((item, index) => (
+                        <HospitalCard
+                            placeType={ item.placeType }
+                            name={ item.name }
+                            tagname={ item.tagname}
+                            distance={ item.distance }
+                            time={ item.time }
+                            isOpen={ item.isOpen }
+                            latitude={ item.latitude }
+                            longitude={ item.longitude}
+                            tel={ item.tel }
+                            current={ curPosition }
+                            key={ index }
+                        />
+                    ))}
+                </div>
+            </section>
+            <section className='hospital-bottomsheet'>
+                
+            </section>
+        </div>
+        </>
+    )
 }
 
-.hospital-hero p {
-    color: var(--color-text-sub1);
-    font-size: var(--font-size-base);
-    margin-left: var(--spacing-xl);
-}
-
-.hospital-search {
-    display: flex;
-    width: 400px;
-    height: 60px;
-    background-color: var(--color-background-search);
-    font-size: var(--font-size-xxl);
-    border: none;
-    border-radius: var(--radius-xxl);
-    align-items: center;
-    margin-right: var(--spacing-xxl);
-}
-
-.hospital-search i {
-    padding: 10px;
-    color: var(--color-text-sub2);
-    cursor: pointer;
-}
-
-.hospital-content {
-    display: flex;
-    min-height: 505px;
-    flex-grow: 1;
-}
-
-.hospital-map {
-    position: relative;
-    width: 50%;
-    height: 588px;
-}
-
-.hospital-legend {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    z-index: var(--z-modal);
-    width: fit-content;
-    height: fit-content;
-    padding: var(--spacing-sm) var(--spacing-lg);
-    background-color: rgb(255, 255, 255, 0.5);
-    border-radius: var(--radius-xl);
-}
-
-.hospital-marker-list > li:nth-child(1)::marker {
-    color: var(--color-firefighter);
-}
-
-.hospital-marker-list > li:nth-child(1) {
-    color: var(--color-firefighter);
-}
-
-.hospital-marker-list > li:nth-child(2)::marker {
-    color: var(--color-medical);
-}
-
-.hospital-marker-list > li:nth-child(2) {
-    color: var(--color-medical);
-}
-
-.hospital-marker-list > li:nth-child(3)::marker {
-    color: var(--color-gover);
-}
-
-.hospital-marker-list > li:nth-child(3) {
-    color: var(--color-gover);
-}
-
-.hospital-marker-list > li:nth-child(4)::marker {
-    color: var(--color-civil);
-}
-
-.hospital-marker-list > li:nth-child(4) {
-    color: var(--color-civil);
-}
-
-.hospital-panel {
-    width: 50%;
-    height: 100%;
-}
-
-.hospital-panel-top {
-    display: flex;
-    justify-content: space-between;
-    padding: 15px 20px;
-}
-
-.hospital-panel-right {
-    display: flex;
-}
-
-.hospital-panel-right h1 {
-    color: var(--color-text-primary);
-    font-size: var(--font-size-xxl);
-    margin-top: var(--spacing-xs);
-}
-
-.hospital-panel-right h3 {
-    display: flex;
-    background-color: var(--color-background-dropdown);
-    color: var(--color-text-third);
-    border: none;
-    border-radius: var(--radius-pill);
-    width: 44px;
-    height: 22px;
-    font-size: var(--font-size-sm);
-    align-items: center;
-    justify-content: center;
-    margin-left: var(--spacing-sm);
-    margin-top: var(--spacing-md);
-}
-
-.hospital-panel-left {
-    display: flex;
-}
-
-.hospital-sort-btn {
-    display: flex;
-    width: 78px;
-    height: 26px;
-    background-color: var(--color-background-search);
-    color: var(--color-text-sub1);
-    border: none;
-    border-radius: var(--radius-pill);
-    align-items: center;
-    justify-content: center;
-    font-size: var(--font-size-sm);
-    margin-top: var(--spacing-md);
-    margin-left: var(--spacing-xs);
-    margin-right: var(--spacing-lg);
-    cursor: pointer;
-}
-
-.active {
-    background-color: var(--color-background-btn);
-    color: var(--color-text-sub3);
-}
-
-.hospital-panel-list {
-    display: flex;
-    justify-content: center;
-    background-color: var(--color-background);
-    border: none;
-    border-radius: var(--spacing-md);
-    margin: 0 auto; 
-    width: 90%;
-    height: 488px;
-    padding-top: var(--spacing-sm);
-    overflow-y: auto;
-}
-
-.hospital-bottomsheet {
-    display: none;
-}
-
-#txt-search {
-    color: var(--color-text-primary);
-    background-color: transparent;
-    border: none;
-    width: 320px;
-    outline: none;
-}
-
-#txt-search::placeholder {
-    color: var(--color-text-sub2);
-}
-
-#btn-search {
-    background-color: var(--color-background-btn);
-    border: none;
-    border-radius: var(--radius-xxl);
-    color: var(--color-text-sub3);
-    width: 60px;
-    height: 40px;
-    margin-right: var(--spacing-md);
-    cursor: pointer;
-}
-
-@media (max-width: 768px) {
-    .hospital-hero {
-        display: none;
-    }
-
-    .hospital-content {
-        width: 100%;
-        height: 100%;
-    }
-
-    .hospital-panel {
-        display: none;
-    }
-}
+export default Hospital;
