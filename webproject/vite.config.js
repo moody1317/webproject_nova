9efe12d38340ca94639ad698facf48200916adee
moody1317 <soen13@chungbuk.ac.kr>
2026-05-24 16:47:36 +0900

    workbox PWA 설정

--- webproject/vite.config.js
+++ webproject/vite.config.js
@@ -8,16 +8,37 @@ export default defineConfig({
     react(),
     VitePWA({
       injectRegister: 'auto',
+      filename: 'sw.js',
       registerType: 'autoUpdate',
       includesAssets: ['**/*'],
       manifest: false,
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,svg,ico,json}'],
-        maximumFileSizeToCaCheInBytes: 8 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 8 * 1024 * 1024,
+        runtimeCaching: [
+          {
+            urlPattern: /\/api\//,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache'
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
       }
     })
   ],
   server: {
+    proxy: {
+      '/api': {
+        target: 'https://jere-trispermous-festively.ngrok-free.dev',
+        changeOrigin: true,
+      }
+    }
+  },
+  preview: {
     proxy: {
       '/api': {
         target: 'https://jere-trispermous-festively.ngrok-free.dev',
