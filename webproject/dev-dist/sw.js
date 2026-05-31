1ff9c077730771e97954f83342b62c62c9d2126a
moody1317 <soen13@chungbuk.ac.kr>
2026-05-31 15:54:50 +0900

    의약품 디자인 수정

--- webproject/dev-dist/sw.js
+++ webproject/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d08cb3c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hfeh5ld0ic8"
+    "revision": "0.21v980k294o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
