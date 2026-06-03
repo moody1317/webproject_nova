ce967c99577533e4b74ec7c0569df05f17a2b585
홍승환 <lap2006@chungbuk.ac.kr>
2026-06-03 23:23:56 +0900

    오타 수정

--- webproject/dev-dist/sw.js
+++ webproject/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-bc322087'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dkqpdmpudf"
+    "revision": "0.krpj6h3g5gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
