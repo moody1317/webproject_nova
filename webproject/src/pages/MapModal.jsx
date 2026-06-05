2cf8aefcf0140b80b52e6eca5986d25e8a739209
Shim soen <soen13@chungbuk.ac.kr>
2026-06-05 22:21:19 +0900

    팝업 삭제

--- webproject/src/pages/MapModal.jsx
+++ webproject/src/pages/MapModal.jsx
@@ -84,8 +84,7 @@ const search = async () => {
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; CARTO'
                                 url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"/>
-                                <Marker position={clickposition ?? curPosition } icon={ currentMarker }>
-                                </Marker>
+                                <Marker position={clickposition ?? curPosition } icon={ currentMarker }></Marker>
                     </MapContainer>
                 </section>    
                 <div className="modal-map-save">
